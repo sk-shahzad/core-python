155300f154e14e0138a90c2f64b81a71fa44decf
sk-shahzad <idealsk3@gmail.com>
2024-06-24 05:03:47 -0700

    OOP practice

--- phy.py
+++ phy.py
@@ -419,26 +419,96 @@ print('python' in course)  #returns boolean value
 # print(p1.welcome())
 
 # Inheritance
-class Car:
-  color = "black"
-  @staticmethod
-  def start():
-    print("car started")
+# class Car:
+#   color = "black"
+#   @staticmethod
+#   def start():
+#     print("car started")
 
-  @staticmethod
-  def stop():
-    Print("car stoped")
+#   @staticmethod
+#   def stop():
+#     Print("car stoped")
 
-class ToyotaCar(Car):
-  def __init__(self, name):
-    self.name = name
+# class ToyotaCar(Car):
+#   def __init__(self, name):
+#     self.name = name
 
-car1 = ToyotaCar("4by4")
+# car1 = ToyotaCar("4by4")
 
-print(car1.name)
-car1.start()
-print(car1.color)
+# print(car1.name)
+# car1.start()
+# print(car1.color)
 
 # Multi-level inheritance have more than one child class.
 # Multiple inheritance have more than one parent class.
 
+# class Complex:
+#   def __init__(self, real, img):
+#     self.real = real
+#     self.img = img
+
+#   def showNumber(self):
+#     print(self.real,"i", self.img,"j")
+# # dunder Function
+#   def __add__(self, num2):            
+#     newReal = self.real + num2.real
+#     newImg = self.img + num2.img
+#     return Complex(newReal, newImg)
+
+# num1 = Complex(1,3)
+# num1.showNumber()
+# num2 = Complex(4, 6)
+# num2.showNumber()
+
+# # num3 = num1.add(num2)
+# num3 = num1 + num2
+# num3.showNumber()
+
+# class Circle:
+#   def __init__(self, radius):
+#     self.radius = radius
+
+#   def area(self):
+#     return (22/7) * self.radius ** 2
+
+#   def perimeter(self):
+#     return 2 * (22/7) * self.radius
+
+# c1 = Circle(21)
+# print(c1.area())
+# print(c1.perimeter())
+
+# class Employee:
+#   def __init__(self, role, dept, salary):
+#     self.role = role
+#     self.dept = dept
+#     self.salary = salary
+
+#   def showDetails(self):
+#       print("role = ", self.role)
+#       print("dept = ", self.dept)
+#       print("salary = ", self.salary)
+
+# class Engineer(Employee):
+#     def __init__(self, name, age):
+#       self.name = name
+#       self.age = age
+#       super().__init__("Engineer", "IT", "75,000")
+
+# # e1 = Employee("manager", "Finance", "60,000")
+# # e1.showDetails()
+# eng1 = Engineer("Elon musk", 40)
+# eng1.showDetails()
+
+class Order:
+  def __init__(self, item, price):
+    self.item = item
+    self.price = price
+
+  def __gt__(self, ordr2):
+    return self.price > ordr2.price
+
+ordr1 = Order("chips", 10)
+ordr2 = Order("tea", 15)
+
+print(ordr1 > ordr2)
