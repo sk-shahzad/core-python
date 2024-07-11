f30ba1ad3ff0398c6365aa761800d88726753d45
sk-shahzad <idealsk3@gmail.com>
2024-07-11 05:57:26 -0700

    Working with templates and views

--- django/home/views.py
+++ django/home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'design with me'},
+    {'id':3, 'name':'Frontend develper'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'saleSawari/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = 1
+    context = {'room': room}
+    return render(request, 'saleSawari/Room.html', context)
--- django/saleSawari/settings.py
+++ django/saleSawari/settings.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'saleSawari.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'Templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
