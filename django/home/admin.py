243bd3fd592a43284de139706d30cea1018a318f
sk-shahzad <shahzadd3v@gmail.com>
2024-07-20 01:08:16 -0700

    differ django folder files

--- django/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Room, Topic, Messege
-
-admin.site.register(Room)
-admin.site.register(Topic)
-admin.site.register(Messege)
